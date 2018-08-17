9ca10a13b7cdaad8b621cc5d091e66483f71b39c
Evan Czaplicki <evancz@users.noreply.github.com>
2018-08-17 15:29:54 -0400

    Switch to style closer to react.js for vue.js code
    No new object literal stuff. Keep it ES3. People can switch to the new way if it suits them easily, but it is harder for folks who do not have the extra compilation set up.

--- vue.js
+++ vue.js
@@ -2,12 +2,13 @@ var Elm = require('dist/elm/main.js');
 var Vue = require('vue');
 
 Vue.component('ElmComponent', {
-  template: `<div ref="node"></div>`,
-  mounted () {
-    Elm.Main.init({
-      node: this.$refs.node
-    })
-  }
+	template: `<div ref="node"></div>`,
+	mounted: function() {
+		var app = Elm.Main.init({
+			node: this.$refs.node
+		});
+		// setup ports here
+	}
 })
 
 // Check out https://github.com/ryannhg/elm-vue for a full example!
